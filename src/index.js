02119b1e994cd674e7fe229d1d98700b7bc7ad38
ice949 <justineimasiku21@gmail.com>
2022-07-22 14:55:35 +0200

    send and get data from api

--- src/index.js
+++ src/index.js
@@ -1,16 +1,47 @@
 import './style.css';
 
-import { scores, displayScores, addScore } from './modules/component.js';
+const refresh = document.getElementById('refresh-btn');
+const gameContainer = document.getElementById('form');
+const newScoreler = document.getElementById('name');
+const scores = document.getElementById('score');
+const listContainer = document.querySelector('.score-list');
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/bNJs0tzt8gXgsvp6cV0o/scores/';
 
-const form = document.getElementById('form');
-const name = document.getElementById('name');
-const score = document.getElementById('score');
-
-window.addEventListener('DOMContentLoaded', () => {
-  scores.forEach(displayScores);
+const renderScore = (gameUsers) => {
+  const gamers = (gameUsers.result);
+  gamers.forEach((gameUser) => {
+    const indScore = document.createElement('h3');
+    indScore.classList.add('ind-score');
+    indScore.innerHTML = `${gameUser.user}:${gameUser.score}`;
+    listContainer.appendChild(indScore);
+  });
+};
+const getScores = async () => {
+  const res = await fetch(url);
+  const data = await res.json();
+  renderScore(data);
+};
+refresh.addEventListener('click', () => {
+  listContainer.innerHTML = ' ';
+  getScores();
 });
-
-form.addEventListener('submit', (e) => {
+const addScoreForm = async () => {
+  const res = fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      user: newScoreler.value,
+      score: scores.value,
+    }),
+  });
+  const data = await (await res).json();
+  newScoreler.value = '';
+  scores.value = '';
+  return data;
+};
+gameContainer.addEventListener('submit', (e) => {
   e.preventDefault();
-  addScore(name, score, displayScores);
+  addScoreForm();
 });
